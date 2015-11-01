f4d40144173b87b068d9f13171d62a8e728ceaf8
Sahar <sahar.ki@gmail.com>
2015-11-01 20:45:29 +0200

    Base code

---
+++ HW1/HW1/SqrMatrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class SqrMatrix
+    {
+
+        public SqrMatrix (int n)
+        {
+
+        }
+
+        static public SqrMatrix inv(SqrMatrix)
+        {
+               invA = inv(A);
+               invDCAB = inv(D-C*invA*B);   
+
+               iA = invA + invA*B*invDCAB*C*invA;
+               iB = -invA*B*invDCAB;
+               iC = -invDCAB*C*invA;
+               iD = invDCAB
+        }
+
+        public void print()
+        {
+
+        }
+    }
+}
---
+++ HW1/HW1/class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class class1
+    {
+
+        public static void Main()
+        {
+
+            SqrMatrix sm = new SqrMatrix(3);
+
+            sm[0, 0] = 2.0;
+            sm[0, 1] = 8.0;
+            sm[1, 0] = 8.0;
+            sm[1, 1] = 2.0;
+            sm[2, 0] = 1.0;
+            sm[2, 2] = 1.0;
+
+            SqrMatrix invSm, tester;
+
+            invSm = SqrMatrix.inv(sm);
+
+            Console.WriteLine("Original Matrix");
+            sm.print();
+            Console.WriteLine("Inverted Matrix");
+            invSm.print();
+
+            Console.WriteLine("Test Matrix");
+            tester = sm * invSm;
+            tester.print();
+
+        } // Main
+
+
+    } // class1
+
+}
