0a21a2e8190c7bfe731d490e861b7cc8c04820d4
saharki <sahar.ki@gmail.com>
2015-11-15 15:46:30 +0200

    a

--- HW2/HW2/Pert.cs
+++ HW2/HW2/Pert.cs
@@ -9,20 +9,61 @@ namespace HW2
     {
         private int n;
         private Arc []arcArr;
+        int[] inDegree;
+        int[] stage_list;
+        int[] new_stage_list;
 
         public Pert(int n)
         {
             // TODO: Complete member initialization
             this.n = n;
+            inDegree = new int[n];
             arcArr = new Arc[n];
+            stage_list = new int[n];
+            new_stage_list = new int[n];
         }
-        public void prerequisite(int p1, int p2)
+        public void prerequisite(int i, int j)
         {
-            throw new NotImplementedException();
+            Arc temp = arcArr[i];
+            arcArr[i] = new Arc();
+            arcArr[i].j = j;
+            arcArr[i].next = temp;
+            inDegree[j]++;
         }
         public void printStages()
         {
+            int flag = 0;
 
+            for(int i=0; i<this.n;i++)
+            {
+                if (inDegree[i]==0)
+                {
+                    stage_list[i]++;
+                    flag = 1;
+                    
+                }
+            }
+    while(flag==1)
+                
+            for (int i = 0; i < this.n; i++)
+            {
+                if (stage_list[i] > 0)
+                {
+                    Console.Out.Write(i +" ");
+                    while (arcArr[i] != null)
+                    {
+                        if((--inDegree[arcArr[i].j])==0)
+                        {
+                            new_stage_list[arcArr[i].j]++;
+                        }
+
+                    }
+                    stage_list[i] = 0;
+                }
+            }
+            int[] temp=stage_list;
+            stage_list = new_stage_list;
+            new_stage_list = temp;
         }
     }
 }
