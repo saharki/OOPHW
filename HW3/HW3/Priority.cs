d735dcbf298dd9ad009ba48fad8d05ed3f84db48
saharki <sahar.ki@gmail.com>
2015-12-03 18:00:04 +0200

    Base code.

---
+++ HW3/HW3/Priority.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW3
+{
+    internal class Priority<T>
+    {
+        private Func<object, object, bool> intIsEqual;
+        private Func<object, object, bool> intIsGreater;
+
+        public Priority(Func<object, object, bool> intIsGreater, Func<object, object, bool> intIsEqual)
+        {
+            this.intIsGreater = intIsGreater;
+            this.intIsEqual = intIsEqual;
+        }
+
+        internal void add(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool removeMax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HW3/HW3/SortedArr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW3
+{
+    class SortedArr<T>
+    {
+        private Func<object, object, bool> doubleIsEqual;
+        private Func<object, object, bool> doubleIsGreater;
+
+        public SortedArr(Func<object, object, bool> doubleIsGreater, Func<object, object, bool> doubleIsEqual)
+        {
+            this.doubleIsGreater = doubleIsGreater;
+            this.doubleIsEqual = doubleIsEqual;
+        }
+
+        internal void add(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void delete(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Geti(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
