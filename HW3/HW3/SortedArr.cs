d3f49705451f13710c0030f83156e1af572d3968
nir <nirey10@gmail.com>
2015-12-06 14:59:52 +0200

    new

--- HW3/HW3/SortedArr.cs
+++ HW3/HW3/SortedArr.cs
@@ -4,28 +4,77 @@ namespace HW3
 {
     class SortedArr<T>
     {
-        private Func<object, object, bool> doubleIsEqual;
-        private Func<object, object, bool> doubleIsGreater;
-
-        public SortedArr(Func<object, object, bool> doubleIsGreater, Func<object, object, bool> doubleIsEqual)
+        private Func<object, object, bool> isEqual;
+        private Func<object, object, bool> isGreater;
+        private Type[] arr;
+        private int indexer=0;
+       private const int arrSize = 8;
+        public SortedArr(Func<object, object, bool> isGreater, Func<object, object, bool> isEqual)
         {
-            this.doubleIsGreater = doubleIsGreater;
-            this.doubleIsEqual = doubleIsEqual;
+            this.isGreater = isGreater;
+            this.isEqual = isEqual;
+            arr = new Type[arrSize];
         }
 
-        internal void add(double v)
+        public void add(Type v)
         {
-            throw new NotImplementedException();
-        }
-         
-        internal void delete(double v)
-        {
-            throw new NotImplementedException();
+            if (indexer >= 8)
+            {
+                return;
+            }
+            Type tmp = null,tmp2=null;
+            for (int i = 0; i < indexer; i++)
+            {
+                if (isGreater(v,arr[i]))
+                {
+                    tmp = arr[i];
+                    arr[i] = v;
+                }
+                else if (tmp!=null)
+                {
+                    tmp2 = arr[i];
+                    arr[i] = tmp;
+
+                }
+            }
+
+            indexer++;
+           
         }
 
-        internal bool Geti(int i)
+        public bool delete(Type v)
         {
-            throw new NotImplementedException();
+            if (indexer ==0)
+            {
+                return false;
+            }
+            int found=0;
+            for (int i=0 ;i<indexer;i++)
+            {
+                if (isEqual(arr[i],v)&&(found==0))
+                {
+                    found = 1;
+                }
+                else if (found==1)
+                {
+                    arr[i-1] = arr[i];
+                }
+            }
+
+            if (found == 0)
+                return false;
+      
+
+            indexer--;
+            return true;  
+          
         }
+
+        public Type Geti(int i)
+        {
+            return arr[i];
+        }
+
+        public int N { get { return arrSize; } }
     }
 }
