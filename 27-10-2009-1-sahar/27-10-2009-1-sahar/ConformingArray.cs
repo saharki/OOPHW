6b8bc3a9e0deccc6faf7c4074611b41e74fdc63d
sahar.ki <sahar.ki@gmail.com>
2016-02-11 20:58:29 +0200

    4

---
+++ 27-10-2009-1-sahar/27-10-2009-1-sahar/ConformingArray.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_10_2009_1_sahar
+{
+    class ConformingArray
+    {
+        private double [] arr;
+		int highest=100;
+		int min,max;
+        public ConformingArray()
+        {
+			arr = new double[100];
+			for(int i=0;i<100;i++)
+			{
+				arr[i]=0;
+			}
+			highest=100;
+			min=-1;
+			max=-1;
+        }
+		
+		private void extend()
+		{
+			highest+=100;
+			double [] temp = new double [highest];
+			for(int i=0;i<highest;i++)
+			{
+				temp[i]=0;
+			}
+			for(int i=0;i<highest-100;i++)
+			{
+				temp[i]=arr[i];
+			}
+			
+			arr=temp;			
+		}
+		
+		public double this[int i]
+		{
+			get
+			{
+				while(i>=highest)
+					extend();
+				
+				return arr[i];
+			}
+			set
+			{
+				while(i>=highest)
+					extend();
+				arr[i]=value;
+                if (value != 0)
+                {
+                    if (i < min || min == -1)
+                        min = i;
+                    if (i > max)
+                        max = i;
+                }
+			}
+			
+		}
+		
+		public int MinIndex
+		{
+			get
+			{
+				return min;
+			}
+		}
+		public int MaxIndex
+		{
+			get
+			{
+				return max;
+			}
+		}
+		
+		public static ConformingArray operator +(ConformingArray A, ConformingArray B)
+		{
+			ConformingArray C = new ConformingArray();
+            int max;
+            if (A.MaxIndex > B.MaxIndex)
+            {
+                max = A.MaxIndex;
+            }
+            else
+                max = B.MaxIndex;
+
+			for(int i=0;i<=max;i++)
+			{
+				C[i]=A[i]+B[i];
+			}
+			
+			return C;
+		}
+
+    }
+}
---
+++ 27-10-2009-1-sahar/27-10-2009-1-sahar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_10_2009_1_sahar
+{
+    public class Conforming_arr
+    {
+        static void Main(string[] args)
+        {
+            ConformingArray arr1, arr2, arr3;
+            int i;
+
+            arr1 = new ConformingArray();
+            arr2 = new ConformingArray();
+
+            for(i=2;i<33;i+=4)
+            {
+                arr1[i] = i * 1.01;
+            }
+            for (i = 3; i < 28; i += 3)
+                arr2[i] = i * 1.001;
+            arr3 = arr1 + arr2;
+
+            Console.WriteLine("arr1.MinIndex = " + arr1.MinIndex);
+            Console.WriteLine("arr1.MaxIndex = " + arr1.MaxIndex);
+            Console.WriteLine("arr2.MinIndex = " + arr2.MinIndex);
+            Console.WriteLine("arr2.MaxIndex = " + arr2.MaxIndex);
+            Console.WriteLine("arr3.MinIndex = " + arr3.MinIndex);
+            Console.WriteLine("arr3.MaxIndex = " + arr3.MaxIndex);
+
+            Console.WriteLine(" arr1:");
+            for (i = arr1.MinIndex; i < arr1.MaxIndex; i++)
+                Console.Write(" " + arr1[i]);
+            Console.WriteLine();
+            Console.Write(" arr2:");
+
+            for (i = arr2.MinIndex; i < arr2.MaxIndex; i++)
+                Console.Write(" " + arr2[i]);
+            Console.WriteLine();
+
+            Console.WriteLine(" arr3:");
+            for (i = arr3.MinIndex; i < arr3.MaxIndex; i++)
+                Console.Write(" " + arr3[i]);
+            Console.WriteLine();
+
+        }
+    }
+}
