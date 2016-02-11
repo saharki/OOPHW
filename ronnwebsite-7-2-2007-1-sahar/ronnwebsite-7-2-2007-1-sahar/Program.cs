1a5ba6991a4963845a79acedbdd48fa0bce79d2b
sahar.ki <sahar.ki@gmail.com>
2016-02-11 17:29:50 +0200

    4

---
+++ ronnwebsite-7-2-2007-1-sahar/ronnwebsite-7-2-2007-1-sahar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ronnwebsite_7_2_2007_1_sahar
+{
+    class new_hash
+    {
+        public static int alt_hash_fun(int x, int n)
+        {
+            return (x * x) % n;
+
+        } // alt_hash_fun
+
+    } //  new_hash
+
+
+    class MainClass
+    {
+        public delegate int ifunc(int x,int n);
+        public static int Main()
+        {
+            array_hash_table My_table = new array_hash_table(7);
+         
+        My_table.insert(39);
+            My_table.insert(5);
+            My_table.insert(23);
+            My_table.insert(9);
+            My_table.insert(3);
+            My_table.insert(2);
+
+
+            Console.WriteLine("\nIs member {0} = {1}", 23L, My_table.is_member(23));
+            Console.WriteLine("\nIs member {0} = {1}", 39L, My_table.is_member(39));
+            Console.WriteLine("\nIs member {0} = {1}", 40L, My_table.is_member(40));
+
+            ifunc new_hash_fun =new ifunc(new_hash.alt_hash_fun);
+ 
+
+            array_hash_table2 My_table2 = new array_hash_table2(9, new_hash_fun);
+
+            My_table2.insert(39);
+            My_table2.insert(5);
+            My_table2.insert(23);
+            My_table2.insert(9);
+            My_table2.insert(3);
+            My_table2.insert(2);
+
+
+            Console.WriteLine("\nIs member {0} = {1}", 23L, My_table2.is_member(23));
+            Console.WriteLine("\nIs member {0} = {1}", 39L, My_table2.is_member(39));
+            Console.WriteLine("\nIs member {0} = {1}", 40L, My_table2.is_member(40));
+
+            return 0;
+
+        } // main
+    } // MainClass
+
+    
+}
