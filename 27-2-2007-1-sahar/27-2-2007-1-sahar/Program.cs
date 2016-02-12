d4833e08c3e8b4d7793f09c223191eb6e3f0e241
sahar.ki <sahar.ki@gmail.com>
2016-02-12 17:41:36 +0200

    4

---
+++ 27-2-2007-1-sahar/27-2-2007-1-sahar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_2_2007_1_sahar
+{
+    class Program
+    {
+        public static int Main()
+        {
+            int x, y;
+            binary_number bn1 = new binary_number();
+            binary_number bn2 = new binary_number();
+            binary_number bn3 = new binary_number();
+
+            double d = 52.8;
+
+            x = 765;
+
+            bn1 = x;
+            bn2 = (binary_number)d;
+
+            Console.WriteLine("bn1 = ");
+            bn1.print();
+            y = bn1;
+            Console.WriteLine("\n value bn1 = {0} ", y);
+
+            Console.WriteLine("\nbn2 = ");
+            bn2.print();
+            y = bn2;
+            Console.WriteLine("\n value bn2 = {0} ", y);
+
+            bn3 = bn1 + bn2;
+            Console.WriteLine("\nbn3 = ");
+            bn3.print();
+            y = bn3;
+            Console.WriteLine("\nvalue bn3 = {0} ", y);
+
+            return 0;
+        } // main
+    } // MainClass
+
+}
+
---
+++ 27-2-2007-1-sahar/27-2-2007-1-sahar/binary_number.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_2_2007_1_sahar
+{
+    class binary_number
+    {
+        int[] arr;
+        int lenght;
+        int num;
+        public binary_number()
+        {
+            arr = new int[128];
+            for (int i = 0; i < 128; i++)
+                arr[i] = 0;
+        }
+        public static implicit operator binary_number( int x)
+        {
+            binary_number temp = new binary_number();
+            int i;
+            temp.num = x;
+            i = 127;
+            while (x != 0)
+            {
+                temp.arr[i] = x % 2;
+                x /= 2;
+                i--;
+            }
+            temp.lenght = 127 - i;
+            return temp;
+        }
+        public static implicit operator int(binary_number x)
+        {
+            return x.num;
+        }
+        public static implicit operator binary_number(double xd)
+        {
+            binary_number temp = new binary_number();
+            int i,x;
+            x = (int)xd;
+            temp.num = x;
+            i = 127;
+            while (x != 0)
+            {
+                temp.arr[i] = x % 2;
+                x /= 2;
+                i--;
+            }
+            temp.lenght = 127 - i;
+            return temp;
+        }
+
+        public static binary_number operator +(binary_number b1, binary_number b2)
+        {
+            binary_number temp = new binary_number();
+            int i, x;
+            x = b1.num + b2.num;
+            temp.num = x;
+            i = 127;
+            while (x != 0)
+            {
+                temp.arr[i] = x % 2;
+                x /= 2;
+                i--;
+            }
+            temp.lenght = 127 - i;
+            return temp;
+        }
+
+        public void print()
+        {
+            for (int i = lenght-1; i >= 0; i--)
+                Console.Write(arr[127 - i]);
+            
+        }
+    }
+}
